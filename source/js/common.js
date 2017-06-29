4c95421c5149350094212ecd797cf493ffe0f759
Nihrom995 <nihrom995@gmail.com>
2017-06-29 18:12:59 +0900

    Site id done. The next is do model form and some php and js

---
+++ source/js/common.js
@@ -0,0 +1,52 @@
+/**
+ * Created by Nihrom on 22.06.2017.
+ */
+$(document).ready(function() {
+    
+    AOS.init({
+       // offset: 240,
+        duration: 600,
+        easing: 'ease-in-sine',
+        delay: 100,
+        once:true,
+    });
+
+    $('body').scrollspy({ target: '#scrollspy', offset:200 })
+
+    // Select all links with hashes
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function(event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function() {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
+        });
+});
